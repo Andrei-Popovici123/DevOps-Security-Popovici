4d5f13b2ada6499cf7c35867b362ff2d3c39d857
Andrei-Popovici123 <andrei-cristian.popovici@student.upt.ro>
2024-09-10 14:53:05 +0200

    Update quoter_templates.py

--- content/quoter_templates.py
+++ content/quoter_templates.py
@@ -1,7 +1,7 @@
 def quote_fragment(id, text, attribution):
     return f"""
 <a href="/quotes/{id}" class="quote img{id % 13}">
-  <q>{text}- nice quote</q>
+  <q>{text}</q>
   <address>{attribution}</address>
 </a>
 """
